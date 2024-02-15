65e957f41a717db6fbfba51695545fa25ccdc77c
pejkob <112940859+pejkob@users.noreply.github.com>
2024-02-15 07:58:39 +0100

    display cards from get-all endpoint

--- src/GetItems.js
+++ src/GetItems.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import axios from 'axios';
 
 function GetItems() {
+
+    const [items,SetItems]=useState([])
+
+      const url="http://nodejs.sulla.hu/data";
+      useEffect(() => {
+        axios.get(url)
+          .then(response => SetItems(response.data))
+          .catch(error => console.error('Hiba a lekérdezés során:', error));
+      }, []);
+
+      const Cardmap = items.map((data, index) => {
+        return (
+          <div key={index} className="card" style={{ width: '18rem' }}>
+            <div className="card-body">
+              <h5 className="card-title">{data.name}</h5>
+              <h6 className="card-subtitle mb-2 text-body-secondary">{data.hostname}</h6>
+              <p className="card-text">{data.location}</p>
+              <p className="card-text">{data.price}</p>
+              <p className="card-text">{data.minimum_nights}</p>
+            </div>
+          </div>
+        );
+      });
+      
   return (
-    <div>GetItems</div>
+    <>
+    {Cardmap}
+    </>
   )
 }
 
